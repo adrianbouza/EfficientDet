f5f4d81b30bd659a226a365d6b751fcbfcc158dc
adrianbouza <66165272+adrianbouza@users.noreply.github.com>
2021-06-15 12:10:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     url=URL,
     version=VERSION,
     packages=find_packages(),
-    ext_modules=cythonize("utils/compute_overlap.pyx"),
+    ext_modules=cythonize("efficientdet/utils/compute_overlap.pyx"),
     include_dirs=[numpy.get_include()]
 )
