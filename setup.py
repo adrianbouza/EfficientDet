afa8934ed42cdf4557436ad7ee8fc647f48fe218
adrianbouza <66165272+adrianbouza@users.noreply.github.com>
2021-06-15 10:45:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ import numpy
 NAME = "efficientdet"
 DESCRIPTION = "EfficientDet model re-implementation. Keras and TensorFlow Keras."
 URL = "https://github.com/adrianbouza/EfficientDet"
-
+VERSION = 1.0.0
 setup(
     name=NAME,
     description=DESCRIPTION,
     url=URL,
+    version=VERSION
     ext_modules=cythonize("utils/compute_overlap.pyx"),
     include_dirs=[numpy.get_include()]
 )
